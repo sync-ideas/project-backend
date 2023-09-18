a905b100edc8ba6c1df223b3e7149e6a9ed621e8
Tabare Bermudez <tabarebermudez@gmail.com>
2023-09-18 08:52:03 -0300

    Add check email functions in auth.route y auth.controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express-serve-static-core';
-import { Prisma } from '@prisma/client';
-
 import { prisma } from '../config/prisma.client.js';
+import jwt from 'jsonwebtoken';
+
 import { AuthHelper } from '../helpers/auth.helper.js';
+import { JWT_SECRET } from '../config/environment.js';
 
 export namespace AuthController {
 
@@ -31,5 +32,47 @@ export namespace AuthController {
         .json({ message: 'Internal server error' });
     }
   }
+
+
+  // Recibe un JWT en el query para comprobar el email
+  export async function checkEmail(req: Request, res: Response) {
+    try {
+      const token = req.query.email
+      if (!token) {
+        return res
+          .status(401)
+          .json({ message: 'Token not found' });
+      }
+      const decodedEmail = jwt.verify(token, JWT_SECRET);
+      let response = await prisma.users.findUnique({
+        where: {
+          email: decodedEmail
+        }
+      })
+      if (response !== null) {
+        response = await prisma.users.update({
+          where: {
+            email: decodedEmail
+          },
+          data: {
+            active: true
+          }
+        })
+        if (response !== null)
+          return res
+            .status(200)
+            .json(response);
+      }
+      return res
+        .status(404)
+        .json({ message: 'User not found' });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: 'Internal server error' });
+    }
+  }
+
 
 }
