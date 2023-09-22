c4776328ca4320b8bc68b7e989f6e8a2cf50f840
Tabare Bermudez <tabarebermudez@gmail.com>
2023-09-22 10:43:11 -0300

    [FEATURE] Register, list and delete students endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 
-import authRouter from './routes/auth.route.js';
 import { PORT } from './config/environment.js';
+
+import authRouter from './routes/auth.route.js';
+import studentsRouter from './routes/students.route.js';
 
 const app = express();
 
 // ---------- Middlewares ----------
+app.use(express.json());
 app.use(cors());
 
 // ---------- Routes ---------------
 app.use('/api', authRouter);
+app.use('/api', studentsRouter);
 
 // ---------- Start server ---------
 app.listen(PORT, () => {
