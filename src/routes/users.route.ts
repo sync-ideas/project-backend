7e26ba561975e20dfb4c1fecda53b0eeb68c84b6
Tabare Bermudez <tabarebermudez@gmail.com>
2023-10-01 19:02:48 -0300

    [REFACTOR] Unify auth and user routes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -4,20 +4,39 @@ import UsersController from '../controllers/users.controller.js';
 const usersRouter = express.Router();
 
 usersRouter.post(
+  '/users/login',
+  UsersController.login
+);
+
+usersRouter.post(
   '/users/register',
   UsersController.register
 );
+
+usersRouter.post(
+  '/users/forgotpassword',
+  UsersController.forgotPassword
+);
+
+usersRouter.post(
+  '/users/resetpassword',
+  UsersController.resetPassword
+)
+
 usersRouter.put(
   '/users/confirm',
   UsersController.confirm
 );
+
 usersRouter.get(
   '/users',
   // middleware de auth, 
   UsersController.getUsers
 );
+
 usersRouter.delete(
   '/users/delete',
+  // middleware de auth,
   UsersController.delete
 );
 
