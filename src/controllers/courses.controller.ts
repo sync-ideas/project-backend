40f53d4f9bc1b85416a4fdc73f61f5d944a6d4e7
SyncIdeas <144357663+sync-ideas@users.noreply.github.com>
2023-10-09 13:24:10 -0300

    Update courses.controller.ts

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -2,21 +2,24 @@ import { Request, Response } from "express-serve-static-core";
 import { prisma } from "../config/prisma.client.js";
 
 const CoursesController = {
+  
   create: async (req: Request, res: Response) => {
     try {
       const { level, number, letter } = req.body;
       if (!level) {
         return res.status(400).json({ message: "Level is required" });
       }
-      let course = await prisma.student.findFirst({
+      let course = await prisma.course.findFirst({
         where: {
           level,
+          number,
+          letter
         },
       });
       if (course) {
         return res.status(400).json({
           result: false,
-          message: "Id already exists",
+          message: "Course already exists",
         });
       }
       course = await prisma.course.create({
@@ -45,6 +48,7 @@ const CoursesController = {
       });
     }
   },
+  
   delete: async (req: Request, res: Response) => {
     try {
       const { level, number, letter } = req.body;
