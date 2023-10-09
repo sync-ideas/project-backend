6d96d6723ac3158a2d5101da4c6bab7a8ec55ad4
SyncIdeas <144357663+sync-ideas@users.noreply.github.com>
2023-10-09 13:20:03 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { PORT } from './config/environment.js';
 import authRouter from './routes/auth.route.js';
 import studentsRouter from './routes/students.route.js';
 import usersRouter from './routes/users.route.js';
-import coursesRouter from "./routes/courses.route.js";
+import coursesRouter from './routes/courses.route.js';
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/api', authRouter);
 app.use('/api', studentsRouter);
 app.use('/api', usersRouter);
-app.use('/api',coursesRouter)
+app.use('/api', coursesRouter)
 
 
 // ---------- Start server ---------
