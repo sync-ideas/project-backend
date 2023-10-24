777c5d63c2584c82d516958f71e11b2feda8ebf2
Tabare Bermudez <tabarebermudez@gmail.com>
2023-10-24 08:50:09 -0300

    [FEATURE] Change required fields in Subjects table

--- build/app.js
+++ build/app.js
@@ -4,6 +4,7 @@ import { PORT } from './config/environment.js';
 import studentsRouter from './routes/students.route.js';
 import usersRouter from './routes/users.route.js';
 import coursesRouter from './routes/courses.route.js';
+import subjectsRouter from './routes/subjects.route.js';
 const app = express();
 // ---------- Middlewares ----------
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cors());
 app.use('/api', studentsRouter);
 app.use('/api', usersRouter);
 app.use('/api', coursesRouter);
+app.use('/api', subjectsRouter);
 // ---------- Start server ---------
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
